01b9555a77fcfd436def52c978ea5bc2346c0f23
Peter <hoffmannpeter82@gmail.com>
2024-05-16 09:12:15 +0200

    Add example for mixin

--- src/lessons/Lesson_01.tsx
+++ src/lessons/Lesson_01.tsx
@@ -1,5 +1,8 @@
 import styles from './Lesson_01.module.scss'
 
 export const Lesson_01: React.FC = ()=>{    
-    return <div className={styles.body}>Hello World</div>
+    return <>
+    <div className={styles.body}>Hello World</div>
+    <div className={styles.warningMessage}>Warning message</div>
+    </>
 }
