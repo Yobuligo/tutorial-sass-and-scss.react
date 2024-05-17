1faf6611f05b6dde2f80ee631bea4def12bdfd08
phoffmann <peter.hoffmann@snpgroup.com>
2024-05-17 11:20:24 +0200

    Add mixin with arguments

--- src/lessons/Lesson_01.tsx
+++ src/lessons/Lesson_01.tsx
@@ -1,8 +1,11 @@
-import styles from './Lesson_01.module.scss'
+import styles from "./Lesson_01.module.scss";
 
-export const Lesson_01: React.FC = ()=>{    
-    return <>
-    <div className={styles.body}>Hello World</div>
-    <div className={styles.warningMessage}>Warning message</div>
+export const Lesson_01: React.FC = () => {
+  return (
+    <>
+      <div className={styles.body}>Hello World</div>
+      <div className={styles.warningMessage}>Warning message</div>
+      <div className={styles.errorMessage}>Error message</div>
     </>
-}
+  );
+};
