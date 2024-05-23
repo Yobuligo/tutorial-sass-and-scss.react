0b3dc0e40fcef3a118a7ea06a76288f9fbfdb220
Peter <hoffmannpeter82@gmail.com>
2024-05-23 08:39:24 +0200

    Add functions

--- src/lessons/Lesson_02.tsx
+++ src/lessons/Lesson_02.tsx
@@ -2,8 +2,13 @@ import styles from "./Lesson_02.module.scss";
 
 export const Lesson_02: React.FC = () => {
   return (
-    <div className={styles.h1}>
-      <h1>Test</h1>
-    </div>
+    <>
+      <div className={styles.h1}>
+        <h1>Test</h1>
+      </div>
+      <div>
+        New Container
+      </div>
+    </>
   );
 };
